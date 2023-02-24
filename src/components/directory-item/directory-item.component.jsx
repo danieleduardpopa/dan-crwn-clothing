839776fabd92eea44f669dafe33df43d713ca602
Daniel Popa <danieleduardpopa@gmail.com>
2023-02-24 23:53:40 +0200

    final touch

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,10 +1,15 @@
+import { useNavigate } from 'react-router-dom';
+
 import { DirectoryItemContainer, BackgroundImage, Body } from './directory-item.styles';
 
 const DirectoryItem = ({ category }) => {
-    const { imageUrl, title } = category;
+    const { imageUrl, title, route } = category;
+    const navigate = useNavigate();
+
+    const navigateHandler = () => navigate(route);
 
     return (
-        <DirectoryItemContainer>
+        <DirectoryItemContainer onClick={navigateHandler}>
           <BackgroundImage imageUrl={imageUrl} />
           <Body>
             <h2>{title}</h2>
